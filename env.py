27af897c57ea890ba0269b282df66056ace7cd39
willcodinggod white <walidwhite@live.co.uk>
2024-06-22 23:13:42 +0000

    added alluth templates and secret key to settings, whitenoise package and dj-database url and connect my databse to the settings and env.py, created static file/css and added siple css in styles.css, colected staticfilesnow deplying again as it didnt work last time

--- env.py
+++ env.py
@@ -1,7 +1,7 @@
 import os
 
 os.environ['SECRET_KEY'] = 'walid1985'
-os.environ['DATABASE_URL'] = 'your-database-url'
+os.environ['DATABASE_URL'] = 'sqlite:///db.sqlite3'
 os.environ['EMAIL_HOST'] = 'your-email-host'
 os.environ['EMAIL_PORT'] = 'your-email-port'
 os.environ['EMAIL_HOST_USER'] = 'your-email-host-user'
