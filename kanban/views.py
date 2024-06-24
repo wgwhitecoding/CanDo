4592ddf6f07ce45505e214cd51ae6c90d1b7ea1d
willcodinggod white <walidwhite@live.co.uk>
2024-06-24 19:20:26 +0000

    added sign up page css and connected it it is fully working

--- kanban/views.py
+++ kanban/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from allauth.account.views import SignupView
+from allauth.account.forms import SignupForm
 
 def index(request):
     return render(request, 'kanban/index.html')
+
+class CustomSignupView(SignupView):
+    template_name = 'account/signup.html'
+    form_class = SignupForm
+
+    def form_valid(self, form):
+        user = form.save(self.request)
+        return redirect('kanban:index')
 
 
