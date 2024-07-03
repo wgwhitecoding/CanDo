0a208209e1e1cdc681360fc0318cb82aac3cdd4e
Walid White <walidwhite@live.co.uk>
2024-07-03 15:54:24 +0000

    Remove env.py from tracking

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ['SECRET_KEY'] = 'walid1985'
-os.environ['DEBUG'] = 'True'
-os.environ['ALLOWED_HOSTS'] = '8000-wgwhitecodi-candocifina-a3thwqraobe.ws-eu114.gitpod.io,cando-ci-6dea2075e664.herokuapp.com'
-os.environ['DATABASE_URL'] = 'postgres://ufzsgyp19rb:AUCgnlylFjFI@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/flip_sharp_exact_989798'
-
